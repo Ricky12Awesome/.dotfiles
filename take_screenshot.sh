e8c3e6381e8f5c50a07ad4e3477afb0178c7b97c
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2024-02-15 04:10:04 -0600

    since slurp waits for user input, should be before getting time of file

--- take_screenshot.sh
+++ take_screenshot.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
 screenshots=Pictures/screenshots
-mkdir -p $HOME/$screenshots 2> /dev/null
-
-filename="$(date +"%Y-%m-%d %H-%M-%S %N").png"
-filepath="$HOME/$screenshots/$filename"
 
 geo=$(slurp -d)
 
 if ! [[ -z $geo ]]; then
+  mkdir -p $HOME/$screenshots 2> /dev/null
+
+  filename="$(date +"%Y-%m-%d %H-%M-%S %N").png"
+  filepath="$HOME/$screenshots/$filename"
+
   grim -g "$geo" -t png - > $filepath
   wl-copy < $filepath
   notify-send --icon "$filepath" -t 4000 "Screenshot taken" "~/$screenshots/$filename" 
